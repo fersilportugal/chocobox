b6dd4124f102c7d773dcdc607d5d32a7a844794a
fersilportugal <46595517+fersilportugal@users.noreply.github.com>
2019-01-25 09:24:28 +0000

    Update gfrecipe.bat

--- gfrecipe.bat
+++ gfrecipe.bat
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://raw.githubusercontent.com/fersilportugal/chocobox/master/gfwks.txt
+START http://boxstarter.org/package/url?https://raw.githubusercontent.com/fersilportugal/chocobox/master/gfrecipe.txt
